283d632eb4dd477f23461fb27b7c241ea52bfca9
Nikita Yrk <49614840+Cheephot@users.noreply.github.com>
2024-05-18 01:07:40 +0300

    added services module

---
+++ app/src/main/java/com/xakaton/wallet/di/ServicesModule.kt
@@ -0,0 +1,46 @@
+package com.xakaton.wallet.di
+
+import com.squareup.moshi.Moshi
+import com.xakaton.wallet.data.remote.services.AuthService
+import com.xakaton.wallet.BuildConfig
+import com.xakaton.wallet.data.remote.services.UsersService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+annotation class UnauthenticatedAuthService
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteServicesModule {
+
+    @Provides
+    @Singleton
+    @UnauthenticatedAuthService
+    fun provideUnauthenticatedAuthService(moshi: Moshi): AuthService {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(AuthService::class.java)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideAuthService(retrofit: Retrofit): AuthService {
+        return retrofit.create(AuthService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUsersService(retrofit: Retrofit): UsersService {
+        return retrofit.create(UsersService::class.java)
+    }
+}
