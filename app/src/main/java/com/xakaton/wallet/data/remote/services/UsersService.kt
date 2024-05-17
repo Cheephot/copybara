6d0bdcea531746fd07aa3582e401ba9a2fe16732
Nikita Yrk <49614840+Cheephot@users.noreply.github.com>
2024-05-18 01:07:12 +0300

    added users service

---
+++ app/src/main/java/com/xakaton/wallet/data/remote/services/UsersService.kt
@@ -0,0 +1,13 @@
+package com.xakaton.wallet.data.remote.services
+
+import com.xakaton.wallet.data.remote.dto.UserDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface UsersService {
+
+    @GET("/api/v1/users/{userId}")
+    suspend fun getUserById(
+        @Path("userId") userId: String
+    ) : UserDto
+}
